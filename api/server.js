41c1bc84cafcbd0b8a549b99bc3379e7d4f801c4
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-04-18 07:30:56 -0400

    Installed nodemon and setup server.listen

--- api/server.js
+++ api/server.js
@@ -3,5 +3,20 @@ const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to my process and actions API - 1st Sprint Challenge",
+    })
+})
+
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        message: "Something went wrong with the request",
+    })
+})
 
 module.exports = server;
